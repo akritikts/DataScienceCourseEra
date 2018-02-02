0d459a1cc49cde87e1a831eb2fe067fc5acd440c
akritikts <akritikts@gmail.com>
2018-02-02 16:05:08 +0530

    bugs resolved

--- mean_pollutant.R
+++ mean_pollutant.R
@@ -1,16 +1,26 @@
 pollutantmean <- function(directory, pollutant, id = 1:332){
-  ndir <- paste("~/RProjects/DataScienceCourseEra/specdata/", directory, ".csv")
-  ndata <- read_csv(ndir)
+  ## Get a list of filenames
+  filenames <- list.files(path=directory, pattern="*.csv")
   
-  if(identical(pollutant, "sulfate"))
-  {
-    nvec <- ndata[2]
-    nvec <- nvec[!is.na(nvec)]
-  }
-  else
-  {
-    nvec <- ndata[3]
-    nvec <- nvec[!is.na(nvec)]
+  ## Loop over the passed id's
+  for(i in id) {
+    
+    ## Pad the i to create a filename
+    filename <- sprintf("%03d.csv", i)
+    filepath <- paste(directory, filename, sep="/")
+    
+    ndata <- read.csv(filepath)
+    nvec <- vector()
+    if(identical(pollutant, "sulfate"))
+    {
+      nvec <- ndata[2]
+      nvec <- nvec[!is.na(nvec)]
+    }
+    else
+    {
+      nvec <- ndata[3]
+      nvec <- nvec[!is.na(nvec)]
+    }
   }
   round(mean(nvec), 3)
 }
